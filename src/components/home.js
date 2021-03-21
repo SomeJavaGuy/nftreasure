2f93ba50f048435c1eef8a224e6f41d4769e8199
Nikos Liakopoulos <nliako@inttrust.gr>
2021-03-21 16:43:52 +0200

    Many riddles

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,12 @@ import React,{Component} from 'react';
 
 class Home extends Component {
   render() {
-    return(<div className='container'><h3>Welcome to the NFTreasure hunt 2021! Add any clues to the end of the URL!</h3></div>
+    
+    return(
+      <div>
+        <link rel="stylesheet" href={process.env.PUBLIC_URL + "/css/landpage.css"} />
+        <div className='bg'></div>
+      </div>    
     )
 }}
 
