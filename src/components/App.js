09d2b3bc8d57782dda250cae770b1186a3ae488d
George Kadianakis <desnacked@riseup.net>
2021-03-21 02:39:51 +0200

    Move StepB2 to StepS2

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Home from "./home";
 import StepP3 from "./StepP3";
 import StepS3 from "./StepS3";
 import StepB3 from "./StepB3";
-import StepB2 from "./StepB2";
+import StepS2 from "./StepS2";
 import MainNavbar from './MainNavbar';
 import About from './about';
 import Web3 from 'web3'
@@ -23,10 +23,10 @@ class App extends Component {
                 {/* <MainNavbar /> */}
 
                 <Switch>
+                  <Route path="/steps2" component={StepS2} />
                   <Route path="/stepp3" component={StepP3} />
                   <Route path="/steps3" component={StepS3} />
                   <Route path="/stepb3" component={StepB3} />
-                  <Route path="/step2" component={StepB2} />
                   <Route path="/about" component={About} />
                   <Route path="/" component={Home} />
                 </Switch>
