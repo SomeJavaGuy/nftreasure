716adc5ad6ac08a4b47e8a221bdac20e68f4cda7
George Kadianakis <desnacked@riseup.net>
2021-03-21 13:45:30 +0200

    More skeleton

---
+++ src/components/StepB1.js
@@ -0,0 +1,26 @@
+import React,{Component} from 'react';
+
+class StepB1 extends Component {
+
+  componentDidMount () {
+    const script = document.createElement("script");
+    script.src = process.env.PUBLIC_URL + "/js/app.js";
+    script.async = true;
+    document.body.appendChild(script);
+}
+
+   render(){
+      return (
+             <div>
+             <title>Where is Wally?</title>
+             Where's wally?
+             <form>
+             <input type='text' id='solution' name='Where is Wally?' />
+             </form>
+             </div>
+      )
+   }
+}
+
+export default StepB1;
+
